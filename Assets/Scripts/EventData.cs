3ae3c2e855331ac18f1334d729415be4b3b3d1ac
Marcel <marcelurpi@gmail.com>
2020-08-09 20:25:22 +0200

    Added EventData ScriptableObject to hold events

---
+++ Assets/Scripts/EventData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class EventData : ScriptableObject
+{
+    [SerializeField] private string eventName = null;
+
+    public string GetName() => eventName;
+}
--- Assets/Scripts/EventPlacer.cs
+++ Assets/Scripts/EventPlacer.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class EventPlacer : MonoBehaviour
 {
     [SerializeField] private int day = 0;
-    [SerializeField] private string eventToPlace = null;
+    [SerializeField] private EventData eventToPlace = null;
     [SerializeField] private Month month = null;
 
     [Button("Place Event", ButtonMode.ActiveOnPlayMode)]
     public void PlaceEvent()
     {
         Cell dayCell = month.GetCellFromDay(day);
-        dayCell.SetContent(eventToPlace);
+        dayCell.SetContent(eventToPlace.GetName());
     }
 }
